74347869e3081fefb9935833f2ed4e812511461e
infinityavoid <80940308+infinityavoid@users.noreply.github.com>
2025-07-18 16:17:16 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -29,28 +29,4 @@ document.addEventListener('DOMContentLoaded', ()=> {
       selectOptions.classList.remove('open');
     }
     });
-  const inputs = document.querySelectorAll('.first-block__content-cell-input');
-  const output = document.querySelector('.first-block__content-cell-output');
-  
-  // Функция для расчета и вывода результата
-  function calculateAndOutput() {
-    const coefficient = parseFloat(inputs[0].value) || 0;
-    const salary = parseFloat(inputs[1].value) || 0;
-    const numberOfSalaries = parseFloat(inputs[2].value) || 0;
-
-    let result = coefficient * salary * numberOfSalaries;
-    if(result < 1) {
-      result = 0
-    }
-    output.textContent = result.toLocaleString('ru-RU'); // Форматируем число в строку с разделением разрядов
-  }
-
-  // Добавляем обработчики событий на поля ввода
-  inputs.forEach((el)=> {
-    el.addEventListener('keydown', (evt) => ["e", "E", "+", "-"].includes(evt.key) && evt.preventDefault())
-    el.addEventListener('input', calculateAndOutput)
-  })
-
-  // Вызываем функцию при загрузке страницы, чтобы инициализировать значение output
-  calculateAndOutput();
 })
