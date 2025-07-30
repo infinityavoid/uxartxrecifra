ccd927d864ad2b656fd50ecb2ccaf1753157192f
infinityavoid <80940308+infinityavoid@users.noreply.github.com>
2025-07-30 10:02:36 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const accordionContainers = document.querySelectorAll('.accordion');
     accordionContainers.forEach((container, index) => {
       container.style.zIndex = accordionContainers.length - index;
+      const content = container.querySelector('.accordion-content');
+
+      if (!content) return;
+
+      if (container.classList.contains('open')) {
+        content.style.height = content.scrollHeight + 'px';
+        content.style.transform = 'translateY(0)';
+      } else {
+        content.style.height = '0px';
+        content.style.transform = `translateY(-${content.scrollHeight}px)`;
+      }
     });
   }
 
@@ -66,4 +77,22 @@ document.addEventListener('DOMContentLoaded', ()=> {
       selectOptions.classList.remove('open');
     }
   });
+  const getCurrentQuaterAndScroll = () => {
+    const gridContainer = document.querySelector('.grid-container');
+    const targetElement = document.querySelector('.grid-header__sub-grid-quater.current');
+    const colStart = document.querySelector('.col-start')
+    const secondBlock = document.querySelector('.second-block')
+    const secondBlockStyles = window.getComputedStyle(secondBlock)
+    const gridContainerStyles = window.getComputedStyle(gridContainer)
+    const gridMarginLeft = gridContainerStyles.getPropertyValue('margin-left')
+    const marginLeft = secondBlockStyles.getPropertyValue('margin-left');
+    const paddingLeft = secondBlockStyles.getPropertyValue('padding-left');
+    if (gridContainer && targetElement) {
+      gridContainer.scrollTo({
+        left: targetElement.offsetLeft - (window.innerWidth > 767 ? colStart.offsetWidth : 0) - parseInt(paddingLeft) - parseInt(marginLeft) - (window.innerWidth <= 767 ? parseInt(gridMarginLeft) : 0),
+        behavior: 'smooth'
+      });
+    }
+  }
+  getCurrentQuaterAndScroll()
 })
